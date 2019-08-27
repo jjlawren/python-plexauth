eb07dfdce44f8ccf761efa98309595e8bdb9f069
Jason Lawrence <jjlawren@users.noreply.github.com>
2019-08-27 14:35:49 -0500

    Fix loading README.md in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='plexauth',
     version='0.0.1',
     description='Handles the authorization flow to obtain tokens from Plex.tv via external redirection.',
-    long_description='README.md',
+    long_description=read('README.md'),
     long_description_content_type='text/markdown',
     url='https://github.com/jjlawren/python-plexauth/',
     license='MIT',
