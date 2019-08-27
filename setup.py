cac839f04a537ff2dcc716ad98a7acfd035404a1
Jason Lawrence <jjlawren@users.noreply.github.com>
2019-08-27 14:45:45 -0500

    Simplify packaging

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
-import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+with open('README.md') as f:
+    long_description = f.read()
+
+VERSION="0.0.1"
 
 setup(
     name='plexauth',
-    version='0.0.1',
+    version=VERSION,
     description='Handles the authorization flow to obtain tokens from Plex.tv via external redirection.',
-    long_description=read('README.md'),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/jjlawren/python-plexauth/',
     license='MIT',
     author='Jason Lawrence',
     author_email='jjlawren@users.noreply.github.com',
-    packages=['plexauth'],
+    platforms='any',
+    py_modules=['plexauth'],
     install_requires=['aiohttp'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
