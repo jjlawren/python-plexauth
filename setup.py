72d7e45b96f5072ecf089c1058cd0585b5dabb20
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2023-12-29 02:55:46 -0500

    make packege typed

--- setup.py
+++ setup.py
@@ -24,5 +24,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ]
+    ],
+    package_data={'plexauth': ['LICENSE', 'README.md', 'py.typed']},
+
 )
